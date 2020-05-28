d576ed5fbd637019aeb8a9ed694e98c4987f86d6
Harry Yu <harryyu1994@gmail.com>
2020-05-28 14:10:31 -0400

    Enable portable AOT on x86
    - Added "-XX:+PortableSharedCache" and "-XX:-PortableSharedCache"
    - Added container detection. By default we enable portableAOT in containers.
    - "-XX:-PortableSharedCache" will disable portableAOT in containers as well.
    - The set of cpu features employed by the SCC is displayed with "-Xshareclasses:printStats"
    
    Issue: #7966
    
    Signed-off-by: Harry Yu <harryyu1994@gmail.com>

--- runtime/shared_common/CacheMap.cpp
+++ runtime/shared_common/CacheMap.cpp
@@ -4509,7 +4509,7 @@ SH_CacheMap::getJavacoreData(J9JavaVM *vm, J9SharedClassJavacoreDataDescriptor* 
 					descriptor->objectBytes -
 					descriptor->debugAreaSize;
 	} else {
-		/* otherBytes does not make sence to multi-layer cache */
+		/* otherBytes does not make sense to multi-layer cache */
 		descriptor->otherBytes = 0;
 	}
 	
@@ -5191,6 +5191,19 @@ SH_CacheMap::printCacheStatsTopLayerStatsHelper(J9VMThread* currentThread, UDATA
 		CACHEMAP_PRINT1(J9NLS_DO_NOT_PRINT_MESSAGE_TAG, J9NLS_SHRC_CM_PRINTSTATS_FEATURE, "default");
 	}
 
+#if defined(J9X86)
+	if (currentThread->javaVM->jitConfig) {
+		j9tty_printf(_portlib, "\t");
+		J9SharedDataDescriptor firstDescriptor;
+		firstDescriptor.address = NULL;
+		findSharedData(currentThread, "J9AOTHeader", sizeof("J9AOTHeader") - 1, J9SHR_DATA_TYPE_AOTHEADER, FALSE, &firstDescriptor, NULL);
+		const size_t BUFF_SIZE = 500;
+		char processorFeatures[BUFF_SIZE];
+		currentThread->javaVM->jitConfig->printAOTHeaderProcessorFeatures(firstDescriptor.address, processorFeatures, BUFF_SIZE);
+		CACHEMAP_PRINT1(J9NLS_DO_NOT_PRINT_MESSAGE_TAG, J9NLS_SHRC_CM_PRINTSTATS_PROCESSOR_FEATURES, processorFeatures);
+	}
+#endif /* defined(J9X86) */
+
 	j9tty_printf(_portlib, "\n");
 	if (true == this->_ccHead->getIsNoLineNumberContentEnabled()) {
 		if (true == this->_ccHead->getIsLineNumberContentEnabled()) {
@@ -5354,6 +5367,7 @@ SH_CacheMap::printCacheStatsAllLayersStatsHelper(J9VMThread* currentThread, UDAT
 	CACHEMAP_FMTPRINT1(J9NLS_DO_NOT_PRINT_MESSAGE_TAG, J9NLS_SHRC_CM_PRINTSTATS_SUMMARY_NUM_STALE_CLASSES_V2, javacoreData->numStaleClasses);
 	CACHEMAP_FMTPRINT1(J9NLS_DO_NOT_PRINT_MESSAGE_TAG, J9NLS_SHRC_CM_PRINTSTATS_SUMMARY_PERC_STALE_CLASSES_V2, javacoreData->percStale);
 }
+
 /**
  * Print stats on an existing cache
  * 
@@ -5424,7 +5438,6 @@ SH_CacheMap::printCacheStats(J9VMThread* currentThread, UDATA showFlags, U_64 ru
 			printCacheStatsTopLayerSummaryStatsHelper(currentThread, showFlags, runtimeFlags, &javacoreData);
 		}
 	}
-
 	return 0;
 }
 
